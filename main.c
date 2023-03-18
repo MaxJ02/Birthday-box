924e7cc5ed99332d4c3b4f459905484dd00c4b5d
Max <114068285+MaxJ02@users.noreply.github.com>
2023-03-18 11:26:24 +0100

    Update main.c

--- main.c
+++ main.c
@@ -61,7 +61,7 @@ void switchSoloLed()
 void setup() {
   Serial.begin(115200);
 
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
